a8374bee95a6c17474e37954ee3dc0aebdaaf402
Shubham Thorat <thoratshubham048@gmail.com>
2022-04-24 23:34:00 +0530

    reload added

--- views/script.js
+++ views/script.js
@@ -1,6 +1,6 @@
 
 async function run(e) {
-    e.preventDefault();
+    // e.preventDefault();
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let enrollno = document.getElementById('enrollno').value;
